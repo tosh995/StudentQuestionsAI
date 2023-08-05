c9f2ac8886d98c5688454cba23e60f386a8cdeb3
tosh995 <vk090133@gmail.com>
2023-08-05 01:31:32 -0400

    Updated app.py

--- app.py
+++ app.py
@@ -1,37 +1,107 @@
-import os
 import streamlit as st
-import OpenAI
+from langchain import PromptTemplate
+from langchain.llms import OpenAI
 
-openai.api_key = os.getenv("API_KEY")
+template = """
+    Below is an email that may be poorly worded.
+    Your goal is to:
+    - Properly format the email
+    - Convert the input text to a specified tone
+    - Convert the input text to a specified dialect
 
-def main():
-    st.title('GPT-4 Question Generator')
+    Here are some examples different Tones:
+    - Formal: We went to Barcelona for the weekend. We have a lot of things to tell you.
+    - Informal: Went to Barcelona for the weekend. Lots to tell you.  
 
-    # Inputs for writing standard name and topic of interest
-    standard_name = st.text_input("Enter the writing standard name:")
-    topic_interest = st.text_input("Enter a topic of personal interest:")
+    Here are some examples of words in different dialects:
+    - American: French Fries, cotton candy, apartment, garbage, cookie, green thumb, parking lot, pants, windshield
+    - British: chips, candyfloss, flag, rubbish, biscuit, green fingers, car park, trousers, windscreen
 
-    # Button to trigger the question generation
-    if st.button('Generate Question'):
-        if standard_name and topic_interest:
-            # Create a prompt for GPT-4
-            prompt = f"Generate a question about {topic_interest} that tests the {standard_name} writing standard."
+    Example Sentences from each dialect:
+    - American: I headed straight for the produce section to grab some fresh vegetables, like bell peppers and zucchini. After that, I made my way to the meat department to pick up some chicken breasts.
+    - British: Well, I popped down to the local shop just the other day to pick up a few bits and bobs. As I was perusing the aisles, I noticed that they were fresh out of biscuits, which was a bit of a disappointment, as I do love a good cuppa with a biscuit or two.
 
-            # Make a request to the OpenAI API
-            response = openai.Completion.create(
-                engine="text-davinci-003",  # as of my knowledge cutoff, this was the latest engine
-                prompt=prompt,
-                temperature=0.6,
-                max_tokens=60
-            )
+    Please start the email with a warm introduction. Add the introduction if you need to.
+    
+    Below is the email, tone, and dialect:
+    TONE: {tone}
+    DIALECT: {dialect}
+    EMAIL: {email}
+    
+    YOUR {dialect} RESPONSE:
+"""
 
-            # Extract the question
-            question = response.choices[0].text.strip()
+prompt = PromptTemplate(
+    input_variables=["tone", "dialect", "email"],
+    template=template,
+)
 
-            # Output the question
-            st.text_area("Generated Question:", question, height=200)
-        else:
-            st.write('Please enter both the writing standard name and topic of interest.')
+def load_LLM(openai_api_key):
+    """Logic for loading the chain you want to use should go here."""
+    # Make sure your openai_api_key is set as an environment variable
+    llm = OpenAI(temperature=.7, openai_api_key=openai_api_key)
+    return llm
 
-if __name__ == "__main__":
-    main()
+st.set_page_config(page_title="Globalize Email", page_icon=":robot:")
+st.header("Globalize Text")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.markdown("Often professionals would like to improve their emails, but don't have the skills to do so. \n\n This tool \
+                will help you improve your email skills by converting your emails into a more professional format. This tool \
+                is powered by [LangChain](https://langchain.com/) and [OpenAI](https://openai.com) and made by \
+                [@GregKamradt](https://twitter.com/GregKamradt). \n\n View Source Code on [Github](https://github.com/gkamradt/globalize-text-streamlit/blob/main/main.py)")
+
+with col2:
+    st.image(image='TweetScreenshot.png', width=500, caption='https://twitter.com/DannyRichman/status/1598254671591723008')
+
+st.markdown("## Enter Your Email To Convert")
+
+def get_api_key():
+    input_text = st.text_input(label="OpenAI API Key ",  placeholder="Ex: sk-2twmA8tfCb8un4...", key="openai_api_key_input")
+    return input_text
+
+openai_api_key = get_api_key()
+
+col1, col2 = st.columns(2)
+with col1:
+    option_tone = st.selectbox(
+        'Which tone would you like your email to have?',
+        ('Formal', 'Informal'))
+    
+with col2:
+    option_dialect = st.selectbox(
+        'Which English Dialect would you like?',
+        ('American', 'British'))
+
+def get_text():
+    input_text = st.text_area(label="Email Input", label_visibility='collapsed', placeholder="Your Email...", key="email_input")
+    return input_text
+
+email_input = get_text()
+
+if len(email_input.split(" ")) > 700:
+    st.write("Please enter a shorter email. The maximum length is 700 words.")
+    st.stop()
+
+def update_text_with_example():
+    print ("in updated")
+    st.session_state.email_input = "Sally I am starts work at yours monday from dave"
+
+st.button("*See An Example*", type='secondary', help="Click to see an example of the email you will be converting.", on_click=update_text_with_example)
+
+st.markdown("### Your Converted Email:")
+
+if email_input:
+    if not openai_api_key:
+        st.warning('Please insert OpenAI API Key. Instructions [here](https://help.openai.com/en/articles/4936850-where-do-i-find-my-secret-api-key)', icon="⚠️")
+        st.stop()
+
+    llm = load_LLM(openai_api_key=openai_api_key)
+
+    prompt_with_email = prompt.format(tone=option_tone, dialect=option_dialect, email=email_input)
+
+    formatted_email = llm(prompt_with_email)
+
+    st.write(formatted_email)
