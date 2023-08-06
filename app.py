c49d106f983e44d3e471ac594a771bc318d2450b
tosh995 <vk090133@gmail.com>
2023-08-06 00:46:11 -0400

    Updated app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ prompt = PromptTemplate(
 
 def load_LLM(openai_api_key):
     """Logic for loading the chain"""
-    llm = OpenAI(maxtokens=-1, temperature=0.6, openai_api_key=openai_api_key)
+    llm = OpenAI(maxtokens?=-1, temperature=0.6, openai_api_key=openai_api_key)
     return llm
     
 
