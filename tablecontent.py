7a6bdb24b8f6e2cdc52b0360a12d38d1eb96a240
tosh995 <vk090133@gmail.com>
2023-08-07 22:08:33 -0400

    Added tablecontent.py

---
+++ tablecontent.py
@@ -0,0 +1,17 @@
+import sqlite3
+import streamlit as st
+import pandas as pd
+
+# Connect to SQLite database
+conn = sqlite3.connect('ai_tool_output.db')
+
+# Read the contents of the 'questions' table into a Pandas DataFrame
+query = "SELECT * FROM questions"
+df = pd.read_sql(query, conn)
+
+# Close the connection
+conn.close()
+
+# Display the DataFrame using Streamlit
+st.write("Contents of the 'questions' table:")
+st.dataframe(df)
