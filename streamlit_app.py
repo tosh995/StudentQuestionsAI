f9b153c7b0cd62980b9e848dd70b5d53de6d6d51
tosh995 <vk090133@gmail.com>
2023-08-08 13:17:03 -0400

    Updated streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -256,42 +256,41 @@ def load_first_input_page():
     global option_count
     global option_standard
     global topic_input
-    if counter==0:
-        st.set_page_config(page_title="AI Questions Generator", page_icon=":robot:")
-        st.header("AI Questions Generator")
+    st.set_page_config(page_title="AI Questions Generator", page_icon=":robot:")
+    st.header("AI Questions Generator")
 
 
-        st.markdown("I am an AI Question Generator Tool. I take a student's topic of interest and Common Core Learning Standard as inputs and generate up to 5 open ended questions for the student to answer. I am powered by [LangChain](https://langchain.com/) and [OpenAI](https://openai.com) ")
+    st.markdown("I am an AI Question Generator Tool. I take a student's topic of interest and Common Core Learning Standard as inputs and generate up to 5 open ended questions for the student to answer. I am powered by [LangChain](https://langchain.com/) and [OpenAI](https://openai.com) ")
 
 
-        st.markdown("## Enter your preferences")
+    st.markdown("## Enter your preferences")
 
 
-        col1, col2 = st.columns(2)
+    col1, col2 = st.columns(2)
 
-        with col1:
-            option_standard = st.selectbox('Which [learning standard](http://www.thecorestandards.org/ELA-Literacy/W/4/) would you like to test?',
-            ('CCSS.ELA-LITERACY.W.4.1', 'CCSS.ELA-LITERACY.W.4.2', 'CCSS.ELA-LITERACY.W.4.3', 'CCSS.ELA-LITERACY.W.4.4','CCSS.ELA-LITERACY.W.4.5','CCSS.ELA-LITERACY.W.4.6','CCSS.ELA-LITERACY.W.4.7', 'CCSS.ELA-LITERACY.W.4.8','CCSS.ELA-LITERACY.W.4.9', 'CCSS.ELA-LITERACY.W.4.10'))
+    with col1:
+        option_standard = st.selectbox('Which [learning standard](http://www.thecorestandards.org/ELA-Literacy/W/4/) would you like to test?',
+        ('CCSS.ELA-LITERACY.W.4.1', 'CCSS.ELA-LITERACY.W.4.2', 'CCSS.ELA-LITERACY.W.4.3', 'CCSS.ELA-LITERACY.W.4.4','CCSS.ELA-LITERACY.W.4.5','CCSS.ELA-LITERACY.W.4.6','CCSS.ELA-LITERACY.W.4.7', 'CCSS.ELA-LITERACY.W.4.8','CCSS.ELA-LITERACY.W.4.9', 'CCSS.ELA-LITERACY.W.4.10'))
 
-        with col2:
-            option_count = st.selectbox('How many questions would you like to generate?',('1','2','3','4','5'))
-
-
-        topic_input = get_topic()
-
-        if len(topic_input.split(" ")) > 6:
-            st.write("Please enter a shorter topic. The maximum length is 6 words.")
-            st.stop()
+    with col2:
+        option_count = st.selectbox('How many questions would you like to generate?',('1','2','3','4','5'))
 
 
+    topic_input = get_topic()
 
-        st.button("*See An Example*", type='secondary', help="Click to see an example of the email you will be converting.", on_click=update_text_with_example)
-
-          
-         
-         
-        st.button("Generate Question", help="Click to generate a question", on_click=start_generate)
+    if len(topic_input.split(" ")) > 6:
+        st.write("Please enter a shorter topic. The maximum length is 6 words.")
+        st.stop()
 
 
- 
-load_first_input_page()
+
+    st.button("*See An Example*", type='secondary', help="Click to see an example of the email you will be converting.", on_click=update_text_with_example)
+
+      
+     
+     
+    st.button("Generate Question", help="Click to generate a question", on_click=start_generate)
+
+
+if counter == 0: 
+    load_first_input_page()
