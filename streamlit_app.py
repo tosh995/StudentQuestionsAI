81535c7b55f62baba77029dc4510dc3f20cd25c0
Tosh995 <vk090133@gmail.com>
2023-08-10 18:28:15 -0400

    Updated streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -649,14 +649,14 @@ def load_feedback_display():
     #return
     #st.stop()
 
-def load_welcome_page():
+def load_welcome_page_initiator():
     st.session_state.session_status='Topic Input'
     
     
 #first function that loads the welcome screen for the tool
 def load_welcome_page():
     st.session_state.session_status='Topic Input'
-    st.header("AI Questions Generator")
+    st.header("AI Questions Generator1")
     st.markdown("I am an AI Question Generator Tool. I take a student's topic of interest and Common Core Learning Standard as inputs and generate open ended questions for the student to answer. I am powered by [LangChain](https://langchain.com/) and [OpenAI](https://openai.com) ")
     st.markdown("## Enter your preferences")
     st.session_state.CCSS_standard = get_CCSS_standard()
