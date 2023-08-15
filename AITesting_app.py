126a71f67bf20d4111a17adda6931e245ed4a1fd
Tosh995 <vk090133@gmail.com>
2023-08-15 01:07:45 -0400

    Updated AITesting_app.py

--- AITesting_app.py
+++ AITesting_app.py
@@ -1077,7 +1077,7 @@ def load_welcome_page():
     st.session_state.feedback_QA_response = ""
     st.session_state.question_last_id = ""
     st.session_state.answer_last_id = ""    
-    st.header("AI Questions Generator")
+    st.header("AI Questions Generator1")
     st.markdown("I am an AI Question Generator Tool. I take a student's topic of interest and Common Core Learning Standard as inputs and generate open ended questions for the student to answer. The student can then submit a response to the question and I will provide feedback to the student's response. ")
     st.markdown("## Enter your preferences")
     st.session_state.CCSS_standard = get_CCSS_standard()
